5d60fd10d2cf0c8424da94cf42a651e349d213ba
Amir.Gem <Amir.Gem@DESKTOP-RTPCI8N>
2023-02-08 17:40:17 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Zoo
+{
+    internal class Program
+    {
+        interface IZoo
+        {
+            string Name();
+            int AnimalsCount();
+            string Address();
+        }
+        interface IAnimal
+        {
+            string AnimalName();
+            string AnimalSound();
+            int AnimalVazn();
+            string AnimalColor();
+        }
+        class ZooKaraj : IZoo
+            {
+            public string Address()
+            {
+                string zooAddress = "karaj";
+                return zooAddress;
+            }
+
+            public int AnimalsCount()
+            {
+                int animalCount = 50;
+                return animalCount;
+            }
+
+            public string Name()
+            {
+                string zooName = "Zoo Karaj";
+                return zooName;
+            }
+
+            class Gorbe : IAnimal
+            {
+                public string AnimalName()
+                {
+                    string animalName = "Gorbe";
+                    return animalName;
+                }
+                public string AnimalSound()
+                {
+                    string animalSound = "Meow";
+                    return animalSound;
+                }
+                public int AnimalVazn() 
+                {
+                    int animalVazn = 5;
+                    return animalVazn;
+                }
+                public string AnimalColor() 
+                {
+                    string animalColor = "Red";
+                    return animalColor;
+                
+                }
+            }
+            }
+        static void Main(string[] args)
+        {
+            Gorbe gorbe = new Gorbe();
+            ZooKaraj zooK = new ZooKaraj();
+            Console.WriteLine(zooK.Name());
+            Console.WriteLine(zooK.Address());
+            Console.WriteLine(zooK.AnimalsCount());
+            
+        }
+            
+    }
+    }
+
+    
